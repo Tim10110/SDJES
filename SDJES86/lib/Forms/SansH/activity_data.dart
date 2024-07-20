9ccb1bb99bb01dc75ef1fd238ac842e6ea9ca9ee
Tim10110 <timothee.loewenguth@gmail.com>
2024-07-20 18:50:35 +0200

    update copyTo

--- SDJES86/lib/Forms/SansH/activity_data.dart
+++ SDJES86/lib/Forms/SansH/activity_data.dart
@@ -2,7 +2,10 @@ class ActiviteData {
   String nomActivite = '';
   int categorie = 1; 
   List<String> typeActivite = []; // Type d'activité pour la catégorie 1
-  String? encadrantNomPrenom; // Nom/prenom de l'encadrant pour catégorie 2 et 3
-  String? encadrantQualif; // Qualification de l'encadrant pour catégorie 2 et 3
+  String? denominationPrestataire;
+  String? encadrantNomPrenom2; // Nom/prenom de l'encadrant pour catégorie 2
+  String? encadrantQualif2;
+  String? encadrantNomPrenom3; // Nom/prenom de l'encadrant pour catégorie 3
+  String? encadrantQualif3; 
   String? numCartePro; // Numero de la carte professionnelle pour catégorie 3
 }
