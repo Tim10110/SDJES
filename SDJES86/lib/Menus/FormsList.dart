ecf419d159391f069132e745237d26a88b0ec33c
Joe C <joeclarence0510@gmail.com>
2024-07-23 00:26:12 +0700

    Upload Word file to external Documents directory if user presses export button

--- SDJES86/lib/Menus/FormsList.dart
+++ SDJES86/lib/Menus/FormsList.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'dart:math';
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_application_2/Forms/SansH.dart';
 import 'package:flutter_application_2/main.dart';
@@ -39,131 +38,27 @@ class _DataState extends State<Data> {
     _retrieveSH();
   }
 
-  void exportTemplateData() async {
-    final PermissionStatus status;
-    final androidInfo = await DeviceInfoPlugin().androidInfo;
-    if(androidInfo.version.sdkInt <= 32) {
-      status = await Permission.storage.request();
-    } else {
-      status = await Permission.manageExternalStorage.request();
-    }
-    if (!status.isGranted) {
-      print('Permission denied');
-      return;
-    }
-    
-    final data = await rootBundle.load('assets/docx/template.docx');
-    final bytes = data.buffer.asUint8List();
-    final docx = await DocxTemplate.fromBytes(bytes);
-
-    final testFileData = await rootBundle.load('assets/Form.png');
-    final testFileContent = testFileData.buffer.asUint8List();
-
-    /*
-    final listNormal = ['Foo', 'Bar', 'Baz'];
-    final listBold = ['ooF', 'raB', 'zaB'];
-
-    final contentList = <Content>[];
-
-    final b = listBold.iterator;
-    for (var n in listNormal) {
-      b.moveNext();
-
-      final c = PlainContent("value")
-        ..add(TextContent("normal", n))
-        ..add(TextContent("bold", b.current));
-      contentList.add(c);
-    }
-    */
-
-    Content c = Content();
-    c
-      //..add(TextContent("docname", "Simple docname"))
-      //..add(TextContent("passport", "Passport NE0323 4456673"))
-      ..add(TableContent("table", [
-        RowContent()
-          ..add(TextContent("key1", "Paul"))
-          ..add(TextContent("key2", "Viberg"))
-          ..add(TextContent("key3", "Engineer"))
-          ..add(ImageContent('img', testFileContent)),
-        RowContent()
-          ..add(TextContent("key1", "Alex"))
-          ..add(TextContent("key2", "Houser"))
-          ..add(TextContent("key3", "CEO & Founder"))
-          ..add(ListContent("tablelist", [
-            TextContent("value", "Mercedes-Benz C-Class S205"),
-            TextContent("value", "Lexus LX 570")
-          ]))
-          ..add(ImageContent('img', testFileContent))
-      ]));
-      /*
-      ..add(ListContent("list", [
-        TextContent("value", "Engine")
-          ..add(ListContent("listnested", contentList)),
-        TextContent("value", "Gearbox"),
-        TextContent("value", "Chassis")
-      ]))
-      ..add(ListContent("plainlist", [
-        PlainContent("plainview")
-          ..add(TableContent("table", [
-            RowContent()
-              ..add(TextContent("key1", "Paul"))
-              ..add(TextContent("key2", "Viberg"))
-              ..add(TextContent("key3", "Engineer")),
-            RowContent()
-              ..add(TextContent("key1", "Alex"))
-              ..add(TextContent("key2", "Houser"))
-              ..add(TextContent("key3", "CEO & Founder"))
-              ..add(ListContent("tablelist", [
-                TextContent("value", "Mercedes-Benz C-Class S205"),
-                TextContent("value", "Lexus LX 570")
-              ]))
-          ])),
-        PlainContent("plainview")
-          ..add(TableContent("table", [
-            RowContent()
-              ..add(TextContent("key1", "Nathan"))
-              ..add(TextContent("key2", "Anceaux"))
-              ..add(TextContent("key3", "Music artist"))
-              ..add(ListContent(
-                  "tablelist", [TextContent("value", "Peugeot 508")])),
-            RowContent()
-              ..add(TextContent("key1", "Louis"))
-              ..add(TextContent("key2", "Houplain"))
-              ..add(TextContent("key3", "Music artist"))
-              ..add(ListContent("tablelist", [
-                TextContent("value", "Range Rover Velar"),
-                TextContent("value", "Lada Vesta SW Sport")
-              ]))
-          ])),
-      ]))
-      ..add(ListContent("multilineList", [
-        PlainContent("multilinePlain")
-          ..add(TextContent('multilineText', 'line 1')),
-        PlainContent("multilinePlain")
-          ..add(TextContent('multilineText', 'line 2')),
-        PlainContent("multilinePlain")
-          ..add(TextContent('multilineText', 'line 3'))
-      ]))
-      ..add(TextContent('multilineText2', 'line 1\nline 2\n line 3'))
-      ..add(ImageContent('img', testFileContent));
-      */
-
-    final d = await docx.generate(c);
-    Directory directory = Directory('/storage/emulated/0/Download');
-    await Directory(directory.path).create(recursive: true);
-    final filepath = '${directory.path}/generated_${Random().nextInt(1000)}.docx';
-    final of = File(filepath);
-    of.createSync();
-    print(of.existsSync());
-    if (d != null) {
-      await of.writeAsBytes(d);
-      print('Document ${of.path} generated! and ${of.existsSync()} exists');
-    }
-  }
-
   void exportData() async {
     if(selectedIndex == -1) {
+      return;
+    }
+
+    bool permissionIsGranted = false;
+    Permission permission = Permission.storage;
+    if(Platform.isAndroid){
+      // Android needs permisssion
+      permissionIsGranted = await permission!.isGranted;
+      if(!permissionIsGranted){
+        await permission.request();
+        permissionIsGranted = await permission.isGranted;
+      }
+    } else if (Platform.isWindows) {
+      // Windows doesn't need permission
+      permissionIsGranted = true;
+    }
+
+    if(!permissionIsGranted) {
+      print('Permission not granted');
       return;
     }
 
@@ -200,22 +95,17 @@ class _DataState extends State<Data> {
     c.add(ImageContent('signature', aMirror.invokeGetter('signature') as Uint8List));
     
     final d = await docx.generate(c);
-    Directory directory = Directory('C:/Users/joecl/OneDrive/Documents/test'); //Directory('/storage/emulated/0/Download');
-    await Directory(directory.path).create(recursive: true);
+    String dirPath = Platform.isWindows ? '${(await getApplicationDocumentsDirectory()).path}/SDJES86' : '/storage/emulated/0/Documents/SDJES86';
+    Directory directory = await Directory(dirPath).create(recursive: true);
     final filepath = '${directory.path}/generated_${Random().nextInt(1000)}.docx';
     final file = File(filepath);
     file.createSync();
     if (d != null) {
       await file.writeAsBytes(d);
       print('Document ${file.path} generated! and ${file.existsSync()} exists');
-      final _ = await sendWordFile(file);
-      deleteSansHData();
+      //deleteSansHData();
     }
   }
-
-  Future<void> sendWordFile(File file) async {
-    
-  } 
 
   void deleteSansHData() {
     if(selectedIndex == -1) {
--- SDJES86/macos/Flutter/GeneratedPluginRegistrant.swift
+++ SDJES86/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
 import file_selector_macos
 import objectbox_flutter_libs
 import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
